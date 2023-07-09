b5d1a703080b3b7682c3f6bb2546f40167b403d6
outrun32 <jakovsap@gmail.com>
2023-07-09 22:08:44 +0300

    added sample unit test, fixed model mapping

---
+++ tests/test_models.py
@@ -0,0 +1,10 @@
+from src.embedding_models.vits8.model import ViTStamp
+import torch
+from PIL import Image
+
+def test_vits8():
+    img_cropped = Image.open("tests/test_data/test_cropped_stamp.png")
+    model = ViTStamp()
+    output = model(image=img_cropped)
+    assert output.shape == torch.Size([1,384]), False
+
