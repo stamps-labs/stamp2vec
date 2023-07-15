7093cfd3091507c04dad9490b1e00be660c23a26
outrun32 <jakovsap@gmail.com>
2023-07-15 11:59:24 +0300

    deleted file

--- test.py
+++
@@ -1,8 +0,0 @@
-from pipelines.segmentation.deeplabv3 import DeepLabv3Pipeline
-from PIL import Image
-
-img = Image.open("dataset_generation/images/cropped_stamps/circle_2_5.png")
-
-model = DeepLabv3Pipeline.from_pretrained("stamps-labs/deeplabv3-finetuned")
-
-print(model(img))
